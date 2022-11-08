8c18137bf885cfa28f7008478106e93b3eef1b2f
Gonçalo Silva <gmiguelgsilva@gmail.com>
2022-11-08 15:24:22 +0000

    update

--- Projeto2022Agar.io_Base/src/game/Game.java
+++ Projeto2022Agar.io_Base/src/game/Game.java
@@ -9,7 +9,7 @@ public class Game extends Observable {
 	//temos de fazer a colocação dos jogadores
 	public static final int DIMY = 30;
 	public static final int DIMX = 30;
-	private static final int NUM_PLAYERS = 80;
+	private static final int NUM_PLAYERS = 90;
 	private static final int NUM_FINISHED_PLAYERS_TO_END_GAME=3;
 
 	public static final long REFRESH_INTERVAL = 400;
